e1b4e37a6b453f2c1407647121954bd9014036a0
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-05-23 14:38:34 +0700

    complete turtle crossing game

--- main.py
+++ main.py
@@ -7,8 +7,29 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+scoreboard = Scoreboard()
+turtle = Player()
+car_manager = CarManager()
+screen.listen()
+screen.onkey(turtle.move, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(turtle) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+    if turtle.finishes():
+        turtle.go_to_start()
+        car_manager.level_up()
+        scoreboard.level_up()
+        scoreboard.update_scoreboard()
+
+screen.exitonclick()
--- player.py
+++ player.py
@@ -1,7 +1,29 @@
+from turtle import Turtle
+import time
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color("black")
+        self.go_to_start()
+        self.shape("turtle")
+        self.setheading(90)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+        print("test")
+
+    def finishes(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
